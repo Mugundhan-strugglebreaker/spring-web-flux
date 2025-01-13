7253a40a99766f6bb208236313fe67334f5f1682
Mugundhan-strugglebreaker <srmvmugund@gmail.com>
2025-01-12 21:27:58 -0800

    Adding Stepverifier Dependency and Testing various cases in Flux

--- spring-reactive-web-flux/src/test/java/org/reactivespring/fluxandmonoplayground/FluxAndmonoTest.java
+++ spring-reactive-web-flux/src/test/java/org/reactivespring/fluxandmonoplayground/FluxAndmonoTest.java
@@ -2,8 +2,9 @@ package org.reactivespring.fluxandmonoplayground;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
-public class FluxAndmonoTest {
+public class FluxAndMonoTest {
 
     @Test
     public void fluxTest(){
@@ -13,7 +14,68 @@ public class FluxAndmonoTest {
         flux.subscribe(System.out::println, (e) -> System.err.println(e),() -> System.out.println("Completed"));
     }
 
+    @Test
     public void fluxTestElement_WithoutError(){
+        Flux<String> stringFlux = Flux.just("spring","spring-boot","reactive-spring").log();
+        StepVerifier.create(stringFlux)
+                .expectNext("spring")
+                .expectNext("spring-boot")
+                .expectNext("reactive-spring")
+                .verifyComplete(); // this verify complete starts the flow if it is not present then flux will not execute
+
+        /*
+         StepVerifier.create(stringFlux)
+                .expectNext("spring")
+                .expectNext("spring-boot1") // Here error throws because value does not matches
+                .expectNext("reactive-spring")
+                .verifyComplete();
+         */
+
+    }
+
+    @Test
+    public void fluxTestElement_WithError(){
+        Flux<String> stringFlux = Flux.just("spring","spring-boot","reactive-spring")
+                .concatWith(Flux.error(new RuntimeException("Default Error Message")))
+                .log();
+
+        // checking expection
+        StepVerifier.create(stringFlux)
+                .expectNext("spring")
+                .expectNext("spring-boot")
+                .expectNext("reactive-spring")
+                .expectError(RuntimeException.class)
+                .verify(); // this verify complete starts the flow if it is not present then flux will not execute
+
+        //checking error message
+        StepVerifier.create(stringFlux)
+                .expectNext("spring")
+                .expectNext("spring-boot")
+                .expectNext("reactive-spring")
+                .expectErrorMessage("Default Error Message")
+                .verify(); // this verify complete starts the flow if it is not present then flux will not execute
+    }
+
+    @Test
+    public void fluxTestElementCount(){
+        Flux<String> stringFlux = Flux.just("spring","spring-boot","reactive-spring")
+                .log();
+
+        // count
+        StepVerifier.create(stringFlux)
+                .expectNextCount(3)
+                .verifyComplete(); // this verify complete starts the flow if it is not present then flux will not execute
+
+    }
+
+    @Test
+    public void fluxTestElements(){
+        Flux<String> stringFlux = Flux.just("spring","spring-boot","reactive-spring")
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNext("spring","spring-boot","reactive-spring")
+                .verifyComplete();
 
     }
 }
